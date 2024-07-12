c586bee8ee7ecedcf0e6aa1cbdcb12002a59cc8a
RaedMazahreh <raed@simplify9.com>
2024-07-12 21:59:34 +0300

    redeploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.mjs
+export default {
+    experimental: {
+        appDir: true, // Enable the app directory
+    },
+    reactStrictMode: true, // Optional: Enable React strict mode
+    swcMinify: true, // Optional: Enable SWC minification for faster builds
+};
